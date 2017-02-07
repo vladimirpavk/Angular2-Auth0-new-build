1966b723fd11b512a551da5f183921a1d4d6a424
Korisnik <vladimirpavk@gmail.com>
2017-02-07 22:31:02 +0100

    Working redirection

--- client/app/AuthGuard2.ts
+++ client/app/AuthGuard2.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
 import { tokenNotExpired } from 'angular2-jwt';
+import { Location } from '@angular/common';
 
 @Injectable()
 export class AuthGuard2 implements CanActivate{
     
-    constructor(private _router: Router){}
+    constructor(private _router: Router,
+                private _location: Location){}
 
     canActivate(){
         
         if(tokenNotExpired()) 
         {   
             console.log('AuthGuard2 true');
-            this._router.navigate['/home'];
+            let path=this._location.path(true);
+            this._router.navigate(['/home']);
             return false;
         }
 
